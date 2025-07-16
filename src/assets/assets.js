1705ee25873cee8187f5ed2561372075e180a636
HashenWatawala <hashanewatawala@gmail.com>
2025-07-16 11:43:48 +0530

    Add icons to sidebar

---
+++ src/assets/assets.js
@@ -0,0 +1,7 @@
+import menu_icon from './menu_icon.png'
+import plus_icon from './plus_icon.png'
+
+export const assets = {
+    menu_icon,
+    plus_icon,
+}
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './Sidebar.css'
+import {assets} from '../../assets/assets.js'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      
+      <div className='top'>
+        <img className='menu' src={assets.menu_icon} alt="" />
+        <div className='new-chat'>
+            <img src={assets.plus_icon} alt="" />
+            <p>New Chat</p>
+        </div>
+        <div className="recent">
+
+        </div>
+      </div>
+      <div className='bottom'>
+        <p className="recent-title">Recent</p>
+        <div className="recent-entry">
+            <img src= alt="" />
+        </div>
+      </div>
     </div>
   )
 }
