762111d9a52879077f750649dfd1e996f9656020
HashenWatawala <hashanewatawala@gmail.com>
2025-07-16 15:44:20 +0530

    Side bar fron-tend created

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,7 +1,15 @@
 import menu_icon from './menu_icon.png'
 import plus_icon from './plus_icon.png'
+import message_icon from './message_icon.png'
+import question_icon from './question_icon.png'
+import settings_icon from './settings_icon.png'
+import history_icon from './history_icon.png'
 
 export const assets = {
     menu_icon,
     plus_icon,
+    message_icon,
+    question_icon,
+    settings_icon,
+    history_icon
 }
